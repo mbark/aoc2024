9b8e0949d96d1eb8211299c59a649830661da431
Martin Wohlfart <github@wohlf.art>
2024-12-09 21:29:27 +0100

    Day 7

--- day7/main.go
+++ day7/main.go
@@ -1,12 +1,96 @@
 package day7
 
-import "fmt"
+import (
+	"fmt"
+	"strings"
+
+	"github.com/mbark/aoc2024/util"
+)
+
+const testInput = `
+190: 10 19
+3267: 81 40 27
+83: 17 5
+156: 15 6
+7290: 6 8 6 15
+161011: 16 10 13
+192: 17 8 14
+21037: 9 7 18 13
+292: 11 6 16 20
+`
 
 func Run(input string, isTest bool) {
-	fmt.Println("This is day7")
+	if isTest {
+		input = testInput
+	}
+
+	var sums []test
+	for _, l := range util.ReadInput(input, "\n") {
+		split := strings.Split(l, ": ")
+		s := util.Str2Int(split[0])
+		parts := util.Str2IntSlice(strings.Split(split[1], " "))
+		sums = append(sums, test{s, parts})
+	}
+
+	fmt.Printf("first: %d\n", first(sums))
+	fmt.Printf("second: %d\n", second(sums))
 }
 
-func main() {
-	// This program does nothing by default
-	fmt.Println("Main function in day7")
+type test struct {
+	sum   int
+	parts []int
+}
+
+func first(sums []test) int {
+	var res int
+	for _, t := range sums {
+		if part1(t.sum, t.parts[0], t.parts[1:]) {
+			res += t.sum
+		}
+	}
+
+	return res
+}
+
+func second(sums []test) int {
+	var res int
+	for _, t := range sums {
+		if part2(t.sum, t.parts[0], t.parts[1:]) {
+			res += t.sum
+		}
+	}
+
+	return res
+}
+
+func part1(target, current int, parts []int) bool {
+	if len(parts) == 0 {
+		return target == current
+	}
+	if current > target {
+		return false
+	}
+
+	p := parts[0]
+	next := parts[1:]
+
+	return part1(target, current+p, next) || part1(target, current*p, next)
+}
+
+func part2(target, current int, parts []int) bool {
+	if len(parts) == 0 {
+		return target == current
+	}
+	if current > target {
+		return false
+	}
+
+	p := parts[0]
+	next := parts[1:]
+
+	concat := util.Str2Int(fmt.Sprintf("%d%d", current, p))
+
+	return part2(target, current+p, next) ||
+		part2(target, current*p, next) ||
+		part2(target, concat, next)
 }
