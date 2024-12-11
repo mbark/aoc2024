cf653243a010965e49484ffe35f2a0b36e4ab893
Martin Wohlfart <github@wohlf.art>
2024-12-11 22:22:06 +0100

    Day 9

--- day9/main.go
+++ day9/main.go
@@ -1,12 +1,118 @@
 package day9
 
-import "fmt"
+import (
+	"fmt"
+	"strings"
+
+	"github.com/mbark/aoc2024/fns"
+	"github.com/mbark/aoc2024/util"
+)
+
+const testInput = `2333133121414131402`
 
 func Run(input string, isTest bool) {
-	fmt.Println("This is day9")
+	if isTest {
+		input = testInput
+	}
+
+	var blocks []*block
+	var id block
+	for i, b := range util.Str2IntSlice(strings.Split(input, "")) {
+		if i%2 == 0 {
+			idd := id
+			blocks = append(blocks, fns.Repeat(&idd, b)...)
+			id++
+		} else {
+			blocks = append(blocks, fns.Repeat[*block](nil, b)...)
+		}
+
+	}
+
+	blocks1 := util.CopyList(blocks)
+	blocks2 := util.CopyList(blocks)
+	fmt.Printf("first: %d\n", first(blocks1))
+	fmt.Printf("second: %d\n", second(blocks2))
 }
 
-func main() {
-	// This program does nothing by default
-	fmt.Println("Main function in day9")
+type block int
+
+func (b *block) String() string {
+	if b == nil {
+		return "."
+	}
+
+	return fmt.Sprintf("%d", *b)
+}
+
+func first(blocks []*block) int {
+	for i := len(blocks) - 1; i >= 0; i-- {
+		for j := 0; j < len(blocks) && j < i; j++ {
+			if blocks[j] == nil {
+				blocks[j] = blocks[i]
+				blocks[i] = nil
+			}
+		}
+	}
+
+	var sum int
+	for i, b := range blocks {
+		if b == nil {
+			continue
+		}
+		sum += i * int(*b)
+
+	}
+	return sum
+}
+
+func second(blocks []*block) int {
+	for i := len(blocks) - 1; i >= 0; i-- {
+		if blocks[i] == nil {
+			continue
+		}
+
+		var size int
+		b := blocks[i]
+		for k := i; k >= 0; k-- {
+			if blocks[k] == nil {
+				break
+			}
+			if *blocks[k] != *b {
+				break
+			}
+			size++
+		}
+
+		for j := 0; j < len(blocks) && j < i; {
+			var k int
+			for ; k < size; k++ {
+				if blocks[j+k] != nil {
+					break
+				}
+			}
+			if k >= size {
+				for k := 0; k < size; k++ {
+					blocks[j+k] = blocks[i-k]
+					blocks[i-k] = nil
+				}
+				break
+			}
+			j += k + 1
+		}
+		i -= size - 1
+	}
+
+	var sum int
+	for i, b := range blocks {
+		if b == nil {
+			continue
+		}
+		sum += i * int(*b)
+
+	}
+	return sum
+}
+
+func debugList(blocks []*block) {
+	fmt.Println(strings.Join(fns.Map(blocks, func(b *block) string { return b.String() }), ""))
 }
--- fns/functools.go
+++ fns/functools.go
@@ -119,3 +119,11 @@ func Values[K comparable, V any](m map[K]V) []V {
 	}
 	return values
 }
+
+func Repeat[T any](t T, n int) []T {
+	arr := make([]T, n)
+	for i := 0; i < n; i++ {
+		arr[i] = t
+	}
+	return arr
+}
