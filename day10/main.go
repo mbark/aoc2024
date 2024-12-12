416c0f4ead8bd0b0bff12e81344ea11ac178a628
Martin Wohlfart <github@wohlf.art>
2024-12-12 12:37:26 +0100

    Day 10

--- day10/main.go
+++ day10/main.go
@@ -1,12 +1,89 @@
 package day10
 
-import "fmt"
+import (
+	"fmt"
+
+	"github.com/mbark/aoc2024/fns"
+	"github.com/mbark/aoc2024/maps"
+)
+
+const testInput = `
+89010123
+78121874
+87430965
+96549874
+45678903
+32019012
+01329801
+10456732`
 
 func Run(input string, isTest bool) {
-	fmt.Println("This is day10")
+	if isTest {
+		input = testInput
+	}
+
+	zeroes := make(map[maps.Coordinate]bool)
+	nines := make(map[maps.Coordinate]bool)
+	m := maps.NewIntMap(input)
+
+	for _, c := range m.Coordinates() {
+		if m.At(c) == 0 {
+			zeroes[c] = true
+		}
+		if m.At(c) == 9 {
+			nines[c] = true
+		}
+	}
+
+	fmt.Println("first: ", first(m, zeroes, nines))
+	fmt.Println("second: ", second(m, zeroes, nines))
 }
 
-func main() {
-	// This program does nothing by default
-	fmt.Println("Main function in day10")
+func first(m maps.Map[int], zeroes, nines map[maps.Coordinate]bool) int {
+	var sum int
+	for z := range zeroes {
+		s1, _ := bfs(z, m, nines)
+		sum += s1
+	}
+	return sum
+}
+
+func second(m maps.Map[int], zeroes, nines map[maps.Coordinate]bool) int {
+	var sum int
+	for z := range zeroes {
+		_, s2 := bfs(z, m, nines)
+		sum += s2
+	}
+	return sum
+}
+
+func bfs(start maps.Coordinate, m maps.Map[int], ends map[maps.Coordinate]bool) (int, int) {
+	queue := []maps.Coordinate{start}
+	visited := map[maps.Coordinate]bool{start: true}
+
+	from := make(map[maps.Coordinate][]maps.Coordinate)
+
+	for len(queue) > 0 {
+		next := queue[0]
+		queue = queue[1:]
+		visited[next] = true
+
+		for _, n := range m.Adjacent(next) {
+			if m.At(n) == m.At(next)+1 {
+				from[n] = append(from[n], next)
+				queue = append(queue, n)
+			}
+		}
+	}
+
+	nines := fns.Filter(fns.Keys(visited), func(c maps.Coordinate) bool {
+		return ends[c]
+	})
+
+	var sum int
+	for _, n := range nines {
+		sum += len(from[n])
+	}
+
+	return len(nines), sum
 }
